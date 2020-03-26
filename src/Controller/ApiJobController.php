fb95bbceb95545aa523f6309bf2e4aa5b9edc543
Fzammit <frederic.zammit@hotmail.fr>
2020-03-26 13:30:01 +0100

    index done :godmode:

--- src/Controller/ApiJobController.php
+++ src/Controller/ApiJobController.php
@@ -2,18 +2,43 @@
 
 namespace App\Controller;
 
+use App\Entity\Job;
+use Doctrine\Common\Annotations\AnnotationReader;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\Encoder\JsonEncoder;
+use Symfony\Component\Serializer\Mapping\Factory\ClassMetadataFactory;
+use Symfony\Component\Serializer\Mapping\Loader\AnnotationLoader;
+use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
+use Symfony\Component\Serializer\Serializer;
 
 class ApiJobController extends AbstractController
 {
+
+    public $serializer;
+
+    public function __construct()
+    {
+        $classMetadataFactory = new ClassMetadataFactory(new AnnotationLoader(new AnnotationReader()));
+        $encoders = [new JsonEncoder()];
+        $normalizers = [new ObjectNormalizer($classMetadataFactory)];
+
+        $this->serializer = new Serializer($normalizers, $encoders);
+    }
+
     /**
-     * @Route("/api/job", name="api_job")
+     * @Route("/api/jobs", name="api_job")
      */
     public function index()
     {
-        return $this->render('api_job/index.html.twig', [
-            'controller_name' => 'ApiJobController',
-        ]);
+        $jobs = $this->getDoctrine()->getRepository(Job::class)->findAll();
+        $data = $this->serializer->normalize($jobs, null, ['groups' => 'all_jobs']);
+        $jsonContent = $this->serializer->serialize($data, 'json');
+
+        $response = new Response($jsonContent);
+        $response->headers->set('Content-Type', 'application/json');
+
+        return $response;
     }
 }
