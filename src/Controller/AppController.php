22a84945379beaa323523832ae7403a5b7d34218
Fzammit <frederic.zammit@hotmail.fr>
2020-03-26 12:27:08 +0100

    ApiControllerCreated :godmode:

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -2,18 +2,19 @@
 
 namespace App\Controller;
 
+use App\Entity\Job;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
 class AppController extends AbstractController
 {
     /**
-     * @Route("/app", name="app")
+     * @Route("/", name="app_index", methods={"GET"})
      */
     public function index()
     {
         return $this->render('app/index.html.twig', [
-            'controller_name' => 'AppController',
+            'jobs' => $this->getDoctrine()->getRepository(job::class)->findAll(),
         ]);
     }
 }
